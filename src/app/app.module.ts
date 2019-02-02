d24444512b784d524291a33b1947a9401d4f6e3e
Harshit Agrawal <harshitag785@gmail.com>
2019-02-02 16:29:20 +0530

    Firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+import { ClientService } from './services/client.service';
+
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent},
+  {path: 'register', component:RegisterComponent},
+  {path:'login', component: LoginComponent}
+];
+
+export const firebaseconfig = {
+  apiKey: "AIzaSyAJmN5tRRWYxuuKjyyErSsz6xrk986Q6iI",
+    authDomain: "clientpanel-2c1d4.firebaseapp.com",
+    databaseURL: "https://clientpanel-2c1d4.firebaseio.com",
+    storageBucket: "clientpanel-2c1d4.appspot.com",
+    messagingSenderId: "431789331651"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PagenotfoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseconfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
